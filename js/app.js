3898d48c7e0c7dd9b38ad74badea5220c3eb5277
wowjdql98 <105402290+wowjdql98@users.noreply.github.com>
2022-07-11 11:25:26 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,40 +1,75 @@
+
+// 검색창 컴포넌트 UI
+$(function(){
+  const html = `
+    <!-- 검색 UI -->
+    <div class="search-box">
+      <form class="search-box-container">
+        <input 
+          type="search" 
+          id="search"
+          placeholder="검색"
+          required
+        >
+        <div class="button-group">
+          <button id="submit" type="submit">
+            <i class="bi bi-search"></i>
+          </button>
+          <button class="btn-close">
+            <i class="bi bi-x-lg"></i>
+          </button>
+        </div>
+      </form>
+    </div>`;
+  $('body').append(html); 
+
+}); 
+
+
 $(function () {
-  //#gnb toggle
-  //1. 열기: #toggle-btn클릭시 #gnb on
-  //2.닫기 : gnb에서 btn-close클릭시 on 사라짐 
+
+  /*** #gnb toggle ***/
+  // 1. 열기: #toggle-btn 클릭시 #gnb on
   $('#toggle-btn').click(function(){
     $('#gnb').addClass('on');
-});
-$('#btn-close').click(function(){
+  });
+  // 2. 닫기: #btn-close 클릭시 #gnb 닫음
+  $('#btn-close').click(function(){
     $('#gnb').removeClass('on');
-});
+  });
 
-});
+  /*** 검색창 열고, 닫기 */
+  // 1. #icon-menu .bi-search 누르면 열기
+  $('#icon-menu .bi-search').click(function(){
+    $(".search-box").addClass('on');
+  });
+  // 2. .search-box .btn-close 누르면 닫기
+  $('.search-box .btn-close').click(function(){
+    $(".search-box").removeClass('on');
+  })
+}); // $
 
-  
 
 
 
 
 
 
-// 서버에 데이터 요청(request)
-function getDate() {
-  
+
+
+
+// 제품 데이터 가져오기
+function getData() {
+  const DataURL = ''
   fetch('https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.json')
   .then(function(res){
     return res.json(); // JSON 객체 변환
   })
   .then(function(obj){
     // obj 동물데이터
-      showProducts(obj);
-      let name = obj[0].name;
-      let category = obj[0].category;
-      let imgUrl = obj[0].imgUrl;
+    showProducts(obj);
   });
-
 }
-
 
 function showProducts(obj) {
   // 현재 페이지의 URL query 파라미터(매개변수)
@@ -42,17 +77,13 @@ function showProducts(obj) {
   console.log(query);
   // ? URL query문을 object(변수)로 변경
   let params = new URLSearchParams(query).get('category');
-    console.log(params);  
-    
 
-    //params ==null 이면 시작페이지 dog출력
-    if (params == null) {
-        params = 'dog'
-    }
+  // params == null 이면(시작 페이지 dog 출력)
+  if(params == null) {
+    params = 'dog'
+  }
+  console.log(params);  
 
-    console.log(params);
-    
-    
   // 동물 데이터 출력
   obj.forEach(function(animal){
     // 카테고리 구분 dog | cat | bird
@@ -64,9 +95,8 @@ function showProducts(obj) {
           <p class="name">${animal.name}</p>
       </div>
     `
-        $('.row').append(html);
-        
-      }
+    $('.row').append(html);
+    }
 
   });
 }
